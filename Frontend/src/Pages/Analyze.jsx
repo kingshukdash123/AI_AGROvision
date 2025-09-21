6d056bf0fa3a40753234e28077b68f0037c540a0
Pratyush <138666886+PratyushPrasun@users.noreply.github.com>
2025-09-21 23:10:36 +0530

    Update Analyze.jsx

--- Frontend/src/Pages/Analyze.jsx
+++ Frontend/src/Pages/Analyze.jsx
@@ -84,7 +84,7 @@ const handleSubmit = async (e) => {
   setLoading(true);
 
   try {
-    const response = await fetch("http://127.0.0.1:8000/predict", {
+    const response = await fetch("https://agrovision-rayp.onrender.com/predict", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
